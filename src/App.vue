32359c5ed74260f11f9511732f54f3ff044aa724
rongsheng <1149864038@qq.com>
2024-10-28 14:21:37 +0800

    增加一个基础配置，添加路由、主页、项目管理工具

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">关于</router-link>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view></router-view>
 </template>
 
 <style scoped>
