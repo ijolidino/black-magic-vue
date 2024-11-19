555a68b864bab85b0a908f4ca4e0b0c9e5d55170
ijolidino <ijunido@gmail.com>
2024-11-19 22:19:09 +0800

    初始版本，页面没有完整

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,14 @@ import router from './router/index'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import store from './store/index.js';
+import Element3 from 'element3';
 createApp(App)
     .use(router)
     .use(ElementPlus)
     .use(store)
+    .use(Element3)
     .mount('#app')
 
+window.onerror = function(e){
+console.log(['https://stackoverflow.com/search?q=[js]+'+e])
+}
